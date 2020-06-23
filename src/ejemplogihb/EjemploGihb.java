6a9d25c136a8b7a75f11ea77d23d1a78788a6625
XaviGames <XaviGames@DESKTOP-G66UF7U.domain.name>
2020-06-23 22:26:52 +0200

    prueba para conectar a gihb en cuenta externa

---
+++ src/ejemplogihb/EjemploGihb.java
@@ -0,0 +1,17 @@
+
+package ejemplogihb;
+
+
+public class EjemploGihb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        System.out.print("saludos");
+    }
+    
+}
